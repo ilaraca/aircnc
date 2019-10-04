cfb6d358ce1c59c28b36d5c6e2e890069d26928d
Ilara Almeida <ilaraalmeida@android-893ee96242bc04a7.localdomain>
2019-10-03 21:55:54 -0300

    criar nova rota de criacao do usuário

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
 
-// req.query = para filtrar
-// req.params = para editar ou deletar
-// req.body = acessar corpo da requisição(criação, edição)
+mongoose.connect('mongodb+srv://ilaraca:090490Ilara@cluster0-gpsrx.mongodb.net/aircnc?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.json());
 app.use(routes);
